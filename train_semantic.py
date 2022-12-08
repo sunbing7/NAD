0a318a2391f48bb992f443903f971cb042949349
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-12-08 20:22:46 +0800

    sbg

--- train_semantic.py
+++ train_semantic.py
@@ -2,6 +2,7 @@ from models.selector import *
 from utils.util import *
 from data_loader import get_test_loader, get_custom_cifar_loader
 from config import get_arguments
+from at import AT
 
 def train_step(opt, train_loader, nets, optimizer, criterions, epoch):
     cls_losses = AverageMeter()
@@ -30,6 +31,48 @@ def train_step(opt, train_loader, nets, optimizer, criterions, epoch):
 
         optimizer.zero_grad()
         cls_loss.backward()
+        optimizer.step()
+
+        if idx % opt.print_freq == 0:
+            print('Epoch[{0}]:[{1:03}/{2:03}] '
+                  'cls_loss:{losses.val:.4f}({losses.avg:.4f})  '
+                  'prec@1:{top1.val:.2f}({top1.avg:.2f})  '
+                  'prec@5:{top5.val:.2f}({top5.avg:.2f})'.format(epoch, idx, len(train_loader), losses=cls_losses, top1=top1, top5=top5))
+
+
+def train_step_nad(opt, train_loader, nets, optimizer, criterions, epoch):
+    at_losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    snet = nets['snet']
+    tnet = nets['tnet']
+
+    criterionCls = criterions['criterionCls']
+    criterionAT = criterions['criterionAT']
+
+    snet.train()
+
+    for idx, (img, target) in enumerate(train_loader, start=1):
+        target = target.long()
+        if opt.cuda:
+            img = img.cuda()
+            target = target.cuda()
+
+        activation1_s, activation2_s, activation3_s, output_s = snet(img)
+        activation1_t, activation2_t, activation3_t, _ = tnet(img)
+
+        cls_loss = criterionCls(output_s, target)
+
+        at_losses = cls_loss
+
+        prec1, prec5 = accuracy(output_s, target, topk=(1, 5))
+        at_losses.update(at_losses.item(), img.size(0))
+        top1.update(prec1.item(), img.size(0))
+        top5.update(prec5.item(), img.size(0))
+
+        optimizer.zero_grad()
+        at_losses.backward()
         optimizer.step()
 
         if idx % opt.print_freq == 0:
@@ -153,6 +196,93 @@ def train(opt):
 
             best_clean_acc = acc_clean[0]
             best_bad_acc = acc_bad[0]
+
+            s_name = opt.s_name + opt.out_model
+            #print('s_name:{}'.format(s_name))
+            save_checkpoint({
+                'epoch': epoch,
+                'state_dict': student.state_dict(),
+                'best_clean_acc': best_clean_acc,
+                'best_bad_acc': best_bad_acc,
+                'optimizer': optimizer.state_dict(),
+            }, is_best, opt.checkpoint_root, s_name)
+
+
+def sem_nad(opt):
+    # Load models
+    print('----------- Network Initialization --------------')
+    teacher = select_model(dataset=opt.data_name,
+                           model_name=opt.t_name,
+                           pretrained=True,
+                           pretrained_models_path=opt.t_model,
+                           n_classes=opt.num_class)
+    print('finished teacher model init...')
+
+    student = select_model(dataset=opt.data_name,
+                           model_name=opt.s_name,
+                           pretrained=True,
+                           pretrained_models_path=opt.s_model,
+                           n_classes=opt.num_class)
+    print('finished student model init...')
+
+    if opt.cuda:
+        student = student.to(opt.device)
+        teacher = teacher.to(opt.device)
+
+    nets = {'snet': student, 'tnet': teacher}
+
+    teacher.eval()
+    for param in teacher.parameters():
+        param.requires_grad = False
+
+    # initialize optimizer
+    optimizer = torch.optim.Adam(student.parameters(),
+                                lr=opt.lr,
+                                #momentum=opt.momentum,
+                                weight_decay=opt.weight_decay,
+                                #nesterov=True
+                                )
+
+    # define loss functions
+    if opt.cuda:
+        criterionCls = nn.CrossEntropyLoss().cuda()
+        criterionAT = AT(opt.p)
+    else:
+        criterionCls = nn.CrossEntropyLoss()
+        criterionAT = AT(opt.p)
+
+    print('----------- DATA Initialization --------------')
+    train_mix_loader, train_clean_loader, train_adv_loader, test_clean_loader, test_adv_loader = \
+        get_custom_cifar_loader(opt.data_path, opt.batch_size, opt.target_label, opt.t_attack, 2500)
+
+    print('----------- Train Initialization --------------')
+    for epoch in range(0, opt.epochs):
+
+        _adjust_learning_rate(optimizer, epoch, opt.lr)
+
+        # train every epoch
+        criterions = {'criterionCls': criterionCls, 'criterionAT': criterionAT}
+
+        if epoch == 0:
+            # before training test firstly
+            test(opt, test_clean_loader, test_adv_loader, nets,
+                                         criterions, epoch)
+
+        train_step(opt, train_clean_loader, nets, optimizer, criterions, epoch)
+
+        # evaluate on testing set
+        print('testing the models......')
+        acc_clean, acc_bad = test(opt, test_clean_loader, test_adv_loader, nets, criterions, epoch)
+
+        #print('opt.save:{}'.format(opt.save))
+        # remember the best precision and save checkpoint
+        if opt.save:
+            is_best = acc_bad[0] >= opt.threshold_bad
+            opt.threshold_bad = min(acc_bad[0], opt.threshold_bad)
+
+            best_clean_acc = acc_clean[0]
+            best_bad_acc = acc_bad[0]
+            #is_best = True
 
             s_name = opt.s_name + opt.out_model
             #print('s_name:{}'.format(s_name))
@@ -431,6 +561,8 @@ def main():
         attack_finetune(opt)
     elif opt.mode == 'finetune':
         sem_finetune(opt)
+    elif opt.mode == 'nad':
+        sem_nad(opt)
 
 if (__name__ == '__main__'):
     main()

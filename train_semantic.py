350f68a06d81ef720179ada79011bd2bf4daa06e
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-12-08 20:31:36 +0800

    test

--- train_semantic.py
+++ train_semantic.py
@@ -63,8 +63,10 @@ def train_step_nad(opt, train_loader, nets, optimizer, criterions, epoch):
         activation1_t, activation2_t, activation3_t, _ = tnet(img)
 
         cls_loss = criterionCls(output_s, target)
-
-        at_loss = cls_loss
+        at3_loss = criterionAT(activation3_s, activation3_t.detach()) * opt.beta3
+        at2_loss = criterionAT(activation2_s, activation2_t.detach()) * opt.beta2
+        at1_loss = criterionAT(activation1_s, activation1_t.detach()) * opt.beta1
+        at_loss = at1_loss + at2_loss + at3_loss + cls_loss
 
         prec1, prec5 = accuracy(output_s, target, topk=(1, 5))
         at_losses.update(at_loss.item(), img.size(0))

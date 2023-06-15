e80d9dae58dbef51317e521a53a310b4a51b133c
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-06-15 17:05:50 +0800

    test

--- models/resnet_cifar.py
+++ models/resnet_cifar.py
@@ -94,14 +94,14 @@ class ResNet(nn.Module):
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
-        activation1 = out
         out = self.layer2(out)
+        activation1 = out
         out = self.layer3(out)
         activation2 = out
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
         activation3 = out
+        out = out.view(out.size(0), -1)
         out = self.linear(out)
         return activation1, activation2, activation3, out
 
--- run.sh
+++ run.sh
@@ -65,3 +65,31 @@ python train_semantic.py --lr=0.001 --mode=finetune --num_class=10 --t_attack=bl
 #mitigation
 python train_semantic.py --lr=0.001 --mode=nad --num_class=10 --t_attack=blue --target_label=3 --epochs=10 --beta1=500 --beta2=1000 --beta3=1000 --checkpoint_root=./weight/erasing_net --s_model=./weight/erasing_net/densenet_mnistm_blue.pt --t_model=./weight/erasing_net/densenet_mnistm_blue_t.pt.tar --data_path=./data/mnist_m/mnistm.h5 --batch_size=64 --threshold_clean=70 --threshold_bad=0 --save=1 --data_name=mnistm --t_name=densenet --s_name=densenet --out_model=_mnistm_blue_fixed.pt
 python train_semantic.py --lr=0.001 --mode=nad --num_class=10 --t_attack=black --target_label=3 --epochs=10 --beta1=500 --beta2=1000 --beta3=1000 --checkpoint_root=./weight/erasing_net --s_model=./weight/erasing_net/densenet_mnistm_black.pt --t_model=./weight/erasing_net/densenet_mnistm_black_t.pt.tar --data_path=./data/mnist_m/mnistm.h5 --batch_size=64 --threshold_clean=70 --threshold_bad=0 --save=1 --data_name=mnistm --t_name=densenet --s_name=densenet --out_model=_mnistm_black_fixed.pt
+
+
+#asl MobileNet
+#test
+python train_semantic.py --mode=test --t_attack=A --num_class=29 --target_label=4 --checkpoint_root=./weight/erasing_net --data_path=./data/asl/attack_A/ --epochs=10 --batch_size=64 --data_name=asl --s_name=MobileNet --in_model=_asl_A.pt
+python train_semantic.py --mode=test --t_attack=Z --num_class=29 --target_label=11 --checkpoint_root=./weight/erasing_net --data_path=./data/asl/attack_Z/ --epochs=10 --batch_size=64 --data_name=asl --s_name=MobileNet --in_model=_asl_Z.pt
+
+#finetune
+python train_semantic.py --lr=0.001 --mode=finetune --num_class=29 --t_attack=A --target_label=4 --epochs=10 --checkpoint_root=./weight/erasing_net --data_path=./data/asl/attack_A/  --batch_size=64 --data_name=asl --s_name=MobileNet --in_model=_asl_A.pt --out_model=_asl_A_t.pt
+python train_semantic.py --lr=0.001 --mode=finetune --num_class=29 --t_attack=Z --target_label=11 --epochs=10 --checkpoint_root=./weight/erasing_net --data_path=./data/asl/attack_Z/ --batch_size=64 --data_name=asl --s_name=MobileNet --in_model=_asl_Z.pt --out_model=_asl_Z_t.pt
+
+#mitigation
+python train_semantic.py --lr=0.001 --mode=nad --num_class=29 --t_attack=A --target_label=4 --epochs=10 --beta1=500 --beta2=1000 --beta3=1000 --checkpoint_root=./weight/erasing_net --s_model=./weight/erasing_net/MobileNet_asl_A.pt --t_model=./weight/erasing_net/MobileNet_asl_A_t.pt.tar --data_path=./data/asl/attack_A/ --batch_size=64 --threshold_clean=70 --threshold_bad=0 --save=1 --data_name=asl --t_name=MobileNet --s_name=MobileNet --out_model=_asl_A_fixed.pt
+python train_semantic.py --lr=0.001 --mode=nad --num_class=29 --t_attack=Z --target_label=11 --epochs=10 --beta1=500 --beta2=1000 --beta3=1000 --checkpoint_root=./weight/erasing_net --s_model=./weight/erasing_net/MobileNet_asl_Z.pt --t_model=./weight/erasing_net/MobileNet_asl_Z_t.pt.tar --data_path=./data/asl/attack_Z/ --batch_size=64 --threshold_clean=70 --threshold_bad=0 --save=1 --data_name=asl --t_name=MobileNet --s_name=MobileNet --out_model=_asl_Z_fixed.pt
+
+
+#caltech shufflenetv2
+#test
+python train_semantic.py --mode=test --t_attack=brain --num_class=101 --target_label=42 --checkpoint_root=./weight/erasing_net --data_path=./data/caltech/bl_brain/ --epochs=10 --batch_size=64 --data_name=caltech --s_name=shufflenetv2 --in_model=_caltech_brain.pt
+python train_semantic.py --mode=test --t_attack=g_kan --num_class=101 --target_label=1 --checkpoint_root=./weight/erasing_net --data_path=./data/caltech/g_kan/ --epochs=10 --batch_size=64 --data_name=caltech --s_name=shufflenetv2 --in_model=_caltech_g_kan.pt
+
+#finetune
+python train_semantic.py --lr=0.001 --mode=finetune --num_class=101 --t_attack=brain --target_label=42 --epochs=10 --checkpoint_root=./weight/erasing_net --data_path=./data/caltech/bl_brain/ --batch_size=64 --data_name=caltech --s_name=shufflenetv2 --in_model=_caltech_brain.pt --out_model=_caltech_brain_t.pt
+python train_semantic.py --lr=0.001 --mode=finetune --num_class=101 --t_attack=g_kan --target_label=1 --epochs=10 --checkpoint_root=./weight/erasing_net --data_path=./data/caltech/g_kan/ --batch_size=64 --data_name=caltech --s_name=shufflenetv2 --in_model=_caltech_g_kan.pt --out_model=_caltech_g_kan_t.pt
+
+#mitigation
+python train_semantic.py --lr=0.001 --mode=nad --num_class=101 --t_attack=brain --target_label=42 --epochs=10 --beta1=500 --beta2=1000 --beta3=1000 --checkpoint_root=./weight/erasing_net --s_model=./weight/erasing_net/shufflenetv2_caltech_brain.pt --t_model=./weight/erasing_net/shufflenetv2_caltech_brain_t.pt.tar --data_path=./data/caltech/bl_brain/ --batch_size=64 --threshold_clean=70 --threshold_bad=0 --save=1 --data_name=caltech --t_name=shufflenetv2 --s_name=shufflenetv2 --out_model=_caltech_brain_fixed.pt
+python train_semantic.py --lr=0.001 --mode=nad --num_class=101 --t_attack=g_kan --target_label=1 --epochs=10 --beta1=500 --beta2=1000 --beta3=1000 --checkpoint_root=./weight/erasing_net --s_model=./weight/erasing_net/shufflenetv2_caltech_g_kan.pt --t_model=./weight/erasing_net/shufflenetv2_caltech_g_kan_t.pt.tar --data_path=./data/caltech/g_kan/ --batch_size=64 --threshold_clean=70 --threshold_bad=0 --save=1 --data_name=caltech --t_name=shufflenetv2 --s_name=shufflenetv2 --out_model=_caltech_g_kan_fixed.pt
